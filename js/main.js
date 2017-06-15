49c7971aad6aeb024683480db44c440dd57e8165
RangiBecerra <rangibecerra@gmail.com>
2017-06-15 17:44:56 -0400

    estructura drag drop

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-//alert("existe");
+//index.html
 function principal() {
     var elemento = document.getElementsByClassName("go")[0];
     elemento.addEventListener("click", accion);
 };
-
+//instagram.html
 function accion() {
     alert("funciona");
 };
+
+function drag(evento){
+	evento.dataTransfer.setData("text", evento.target.id);
+}
+function finalDrop(evento) {
+	// resetear navegador 
+	evento.preventDefault();
+}
+function drop(evento) {
+	// resetear navegador 
+	evento.preventDefault();
+	// traer datod de Drag
+	var dato = evento.dataTransfer.getData("text");
+	// objetivo(target) hijo de dato(navegando)
+	evento.target.appendChild(document.getElementById(dato));
+}
