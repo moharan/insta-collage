0c2e6694674539a2db9ba73bb79c3e4a289ba044
RangiBecerra <rangibecerra@gmail.com>
2017-06-15 13:51:34 -0400

    instagram page 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("existe");
+//alert("existe");
+function principal() {
+    var elemento = document.getElementsByClassName("go")[0];
+    elemento.addEventListener("click", accion);
+};
+
+function accion() {
+    alert("funciona");
+};
